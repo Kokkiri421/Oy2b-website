b70656ee5afb1fbe38ad72a16e8fe4ac2b65c0fc
e.fishev <e.fishev@oy2b.ru>
2021-03-12 17:15:35 +0300

    added dev and started forms

---
+++ components/DefaultLayout/OrderBlock.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper order">
+    <div class="order-wrapper">
+      <div class="order-block">
+        <h4 class="order-block__header">
+          Получите бесплатную консультацию специалиста
+          <br />
+          в области бизнес провайдинга:
+        </h4>
+        <div class="order-block__form-container">
+          <form class="order-block__form">
+            <div clas="order-block__form-item"></div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OrderBlock',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/media_mixin';
+.order-wrapper {
+  padding: 3em 0;
+  .order-block {
+    background-color: #f2f2f2;
+    border-radius: 6px;
+    padding: 2.75em 3em;
+    @include _600 {
+      padding: 1em 1.2em;
+    }
+
+    &__header {
+    }
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,9 @@
       <default-header />
     </header>
     <div class="flex-grow-1 content"><Nuxt /></div>
-    <div class="flex-grow-1 order">order</div>
+    <div class="flex-grow-1">
+      <order-block />
+    </div>
     <footer class="flex-grow-1">
       <default-footer />
     </footer>
@@ -14,6 +16,7 @@
 <script>
 import DefaultHeader from '~/components/DefaultLayout/DefaultHeader'
 import DefaultFooter from '~/components/DefaultLayout/DefaultFooter'
+import OrderBlock from '~/components/DefaultLayout/OrderBlock'
 export default {
   data() {
     return {
@@ -24,6 +27,7 @@ export default {
   components: {
     DefaultHeader,
     DefaultFooter,
+    OrderBlock,
   },
   methods: {
     updateScroll() {
@@ -57,9 +61,6 @@ body {
   .content {
     height: 800px;
     background: #706e67;
-  }
-  .order {
-    height: 500px;
   }
   display: flex;
   flex-direction: column;
@@ -130,6 +131,53 @@ button {
   &:focus {
     outline: none;
     box-shadow: none;
+  }
+}
+form {
+  .pretty-input {
+    position: relative;
+    display: inline-block;
+    vertical-align: top;
+    width: 23%;
+    margin: 0 2% 1em 0;
+    .form-input {
+      appearance: none;
+      padding: 0.8em 1em;
+      border: 1px solid #fff;
+      border-radius: 4px;
+      min-width: 280px;
+      line-height: 1.6em;
+      min-height: 3.2em;
+      box-sizing: border-box;
+      resize: none;
+      font: 400 1em/1.1em Montserrat, sans-serif;
+      &__placeholder {
+        position: absolute;
+        line-height: 1.6em;
+        left: 1em;
+        top: 0;
+        color: #999;
+        font-size: 1em;
+        padding: 0 0.3em;
+        transform: scale(1) translateY(0.8em);
+        transition: transform 0.2s ease-out;
+        transform-origin: 0 0;
+        pointer-events: none;
+        border-radius: 4px;
+      }
+    }
+  }
+}
+h4 {
+  font-size: 2em;
+  margin: 0 0 0.7em 0;
+  font-weight: 700;
+  line-height: normal;
+  @include _1300() {
+    font-size: 1.5em;
+  }
+  @include _700() {
+    font-size: 1.2em;
   }
 }
 </style>
