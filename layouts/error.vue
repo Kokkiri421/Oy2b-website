d32d9edfb8069427318e13dc5d9d5075609f74c9
e.fishev <e.fishev@oy2b.ru>
2021-03-26 13:58:25 +0300

    added error page

---
+++ layouts/error.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="error-container">
+    <div class="error">
+      <p class="error__code" v-if="error.statusCode === 404">404</p>
+      <p class="error__message" v-if="error.statusCode === 404">
+        Страница, которую вы&nbsp;ищете, не&nbsp;найдена.
+      </p>
+      <p class="error__message" v-else>Произошла неизвестная ошибка.</p>
+      <NuxtLink to="/">
+        <button class="error__button dialog-button">
+          Вернуться на главную
+        </button>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'errorLayout',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/media_mixin';
+@import '~/assets/common-style';
+.error-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  margin-bottom: 10em;
+  align-items: center;
+  .error {
+    flex-grow: 1;
+    max-width: 320px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    margin-bottom: 10em;
+    &__code {
+      color: #fff;
+      font-size: 8em;
+      line-height: normal;
+      text-align: center;
+      display: none;
+      @include _900() {
+        display: block;
+      }
+    }
+    &__message {
+      color: #fff;
+      font-size: 1.6em;
+      line-height: normal;
+      padding-bottom: 1em;
+      text-align: center;
+    }
+    &__button {
+      width: 100%;
+    }
+    @include _900() {
+      justify-content: center;
+      margin-bottom: 0;
+    }
+  }
+}
+</style>
---
+++ layouts/errorLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="error-background">
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'errorLayout',
+}
+</script>
+
+<style lang="scss">
+@import '~/assets/media_mixin';
+@import '~/assets/common-style';
+.error-background {
+  height: 100vh;
+  max-height: 100vh;
+  overflow: hidden;
+  background-image: url('~/static/images/backgrounds/404-bg.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-color: #111;
+  @include _900() {
+    background-image: none;
+  }
+}
+</style>
