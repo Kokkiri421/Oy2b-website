4fddd3f8ab8183a9742abce02bc8220b6ef8ac17
e.fishev <e.fishev@oy2b.ru>
2021-04-30 16:58:14 +0300

    small bugfix

--- middleware/yMetrics.js
+++ middleware/yMetrics.js
@@ -4,6 +4,7 @@ export default function (context) {
   if (process.browser) {
     if (typeof ym === 'function') {
       setTimeout(async () => {
+        console.log(context.route.path)
         await ym(42010699, 'hit', context.route.path)
       }, 0)
     }
