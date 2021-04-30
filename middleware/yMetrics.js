057a1abba8aea336861bef66d0d15683154e6b17
e.fishev <e.fishev@oy2b.ru>
2021-04-30 16:42:09 +0300

    small bugfix

--- middleware/yMetrics.js
+++ middleware/yMetrics.js
@@ -2,10 +2,10 @@ export default function (context) {
   // Add the userAgent property to the context
   //console.log(context.route.path)
   if (process.browser) {
-    setTimeout(() => {
-      if (typeof ym !== undefined) {
-        ym(42010699, 'hit', context.route.path)
-      }
-    }, 200)
+    if (typeof ym === 'function') {
+      setTimeout(async () => {
+        await ym(42010699, 'hit', context.route.path)
+      }, 0)
+    }
   }
 }
