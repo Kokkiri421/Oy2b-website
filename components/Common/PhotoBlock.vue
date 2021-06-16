af2a5f2abc295022bd7ec9cb39f64fc7958a7e39
e.fishev <e.fishev@oy2b.ru>
2021-06-16 16:04:08 +0300

    testing photo block

--- components/Common/PhotoBlock.vue
+++ components/Common/PhotoBlock.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="photo-block">
     <div class="photo-block__img">
-      <img src="~/static/portraits/roman.png" class="img img__1"/>
-      <img src="~/static/portraits/roman.png" class="img img__2"/>
+      <img src="~/static/portraits/roman.png" class="img img__1" />
+      <img src="~/static/portraits/roman.png" class="img img__2" />
     </div>
     <div class="photo-block__description">
       <img src="~/static/portraits/roman.png" />
@@ -13,19 +13,19 @@
 <style lang="scss" scoped>
 @import '~/assets/media_mixin';
 .photo-block {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  justify-content: space-between;
   width: 500px;
   height: 600px;
   &__img {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: space-between;
     .img {
       width: 400px;
       height: 500px;
       flex-basis: 400px;
     }
-    
+
     width: 500px;
     height: 500px;
   }
