46cfdc74759f311d907ad51c03727e0b2a463099
e.fishev <e.fishev@oy2b.ru>
2021-06-16 15:59:06 +0300

    testing photo block

--- components/Common/PhotoBlock.vue
+++ components/Common/PhotoBlock.vue
@@ -13,18 +13,16 @@
 <style lang="scss" scoped>
 @import '~/assets/media_mixin';
 .photo-block {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
   width: 500px;
   height: 600px;
   &__img {
     .img {
       width: 400px;
       height: 500px;
-      &__1 {
-        left: 0%;
-      }
-      &__2 {
-        right: 0%;
-      }
+      flex-basis: 400px;
     }
     
     width: 500px;
