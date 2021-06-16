d481848232549a3f221152b3b5b34358024a36d3
e.fishev <e.fishev@oy2b.ru>
2021-06-16 14:14:02 +0300

    testing photo block

--- components/Common/PhotoBlock.vue
+++ components/Common/PhotoBlock.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="photo-block">
     <div class="photo-block__img">
-      <img src="~/static/portraits/roman.png" />
+      <img src="~/static/portraits/roman.png" class="img1"/>
     </div>
     <div class="photo-block__description">
       <img src="~/static/portraits/roman.png" />
@@ -11,4 +11,12 @@
 <script></script>
 <style lang="scss" scoped>
 @import '~/assets/media_mixin';
+.photo-block {
+  &__img {
+    .img1 {
+      width: 200px;
+      height: 800px;
+    }
+  }
+}
 </style>
