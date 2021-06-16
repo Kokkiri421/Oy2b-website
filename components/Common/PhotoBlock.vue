b443ee98c3b3c4bbc3e5cc8b06debced20630222
e.fishev <e.fishev@oy2b.ru>
2021-06-16 16:24:50 +0300

    testing photo block

--- components/Common/PhotoBlock.vue
+++ components/Common/PhotoBlock.vue
@@ -2,10 +2,10 @@
   <div class="photo-block">
     <div class="photo-block__img">
       <img src="~/static/portraits/roman.png" class="img img__1" />
-      <img src="~/static/portraits/roman.png" class="img img__2" />
+      <img src="~/static/portraits/ivan.png" class="img img__2" />
     </div>
     <div class="photo-block__description">
-      <img src="~/static/portraits/roman.png" />
+      
     </div>
   </div>
 </template>
